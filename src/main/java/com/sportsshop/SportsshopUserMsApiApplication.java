1082b3f4287dcf5e692e8e8eabce3e11ade54470
manojkumar9908 <manojroyal9908@gmail.com>
2022-05-05 17:23:16 +0530

    add src pom.xml

---
+++ src/main/java/com/sportsshop/SportsshopUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.sportsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsshopUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsshopUserMsApiApplication.class, args);
+		System.out.println("user ms is working");
+	}
+
+}
---
+++ src/test/java/com/sportsshop/SportsshopUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsshopUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
